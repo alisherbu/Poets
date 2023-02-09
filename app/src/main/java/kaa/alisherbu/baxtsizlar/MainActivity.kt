c91c3ec7a38d6071315b51e2982687ba5119576e
AlisherDev <alisherqazaqbaev@gmail.com>
2023-02-09 23:53:43 +0500

    koin2

--- app/src/main/java/kaa/alisherbu/baxtsizlar/MainActivity.kt
+++ app/src/main/java/kaa/alisherbu/baxtsizlar/MainActivity.kt
@@ -5,15 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import com.github.terrakok.cicerone.Command
 import com.github.terrakok.cicerone.Navigator
+import com.github.terrakok.cicerone.NavigatorHolder
 import com.github.terrakok.cicerone.Replace
 import com.github.terrakok.cicerone.androidx.AppNavigator
 import com.github.terrakok.cicerone.androidx.FragmentScreen
-import kaa.alisherbu.baxtsizlar.navigation.NavigationModule
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    private val navigator: Navigator = AppNavigator(this, R.id.content_container)
-    private val navigatorHolder = NavigationModule.navigatorHolder()
+    private val navigator: Navigator  = AppNavigator(this, R.id.content_container)
+    private val navigatorHolder: NavigatorHolder by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
